f23a1c2b65e0f59f75e4e8fc53be1745d3d02722
ProfOak <openprofoak@gmail.com>
2015-07-03 10:21:33 -0500

    added option to change dotexclude file

--- dotstribute.py
+++ dotstribute.py
@@ -3,21 +3,38 @@ import os
 from optparse import OptionParser
 
 def main():
+    parser = OptionParser()
+    parser.add_option("-d", "--dotexe", dest = "dot_exclude",
+            help = "Exlucde files, given by .dotexclude file")
+    parser.add_option("-f", "--force", dest = "force", default = False,
+            action = "store_true", help = "Force overwrite the previus links")
+
+    (options, args) = parser.parse_args()
+
     # add option: add files for exlusion
     # don't symlink these, because they are not your dotfiles (I hope)
-    EXCLUDE = ["license", ".git", ".gitignore", "readme.md"]
+    EXCLUDE = ["LICENSE", ".git", ".gitignore", "README.md"]
 
-    # add option: custom file
+    # option for custom dotexclude file
     dotexclude = ".dotexclude"
+    if options.dot_exclude:
+        dotexclude = options.dot_exclude
+
+    # prepare list of files not to link to $HOME
     if os.path.exists(dotexclude):
         with open(dotexclude) as f:
             EXCLUDE += f.read().split()
     EXCLUDE.append(dotexclude)
 
+    print "EXCLUDE:", EXCLUDE
+    print args
+
     # add option: operatate from any directory
     # maybe no flags = current dir
-    files = [f for f in os.listdir(".") if f.lower() not in EXCLUDE]
+    # get files not in exclude list
+    files = [f for f in os.listdir(".") if f not in EXCLUDE]
 
+    # by default place dotfiles in your $HOME
     for f in files:
         to = os.environ["HOME"] + "/"
         if not f.startswith("."):
